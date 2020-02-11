5bb3898d3ba3efe2c689ea0efec10cf909c9ad63
apple <apple@appledeMacBook-Air-36.local>
2020-02-11 00:51:35 +0000

    CLI

--- MScIT_TeamProject_TemplateProject/src/commandline/TopTrumpsCLIApplication.java
+++ MScIT_TeamProject_TemplateProject/src/commandline/TopTrumpsCLIApplication.java
@@ -1,15 +1,61 @@
 package commandline;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Top Trumps command line application
  */
 public class TopTrumpsCLIApplication {
 
+
+	static Map<Integer,String[]> arrs=new HashMap<Integer,String[]>();
+	static String attrs[];
 	/**
 	 * This main method is called by TopTrumps.java when the user specifies that they want to run in
 	 * command line mode. The contents of args[0] is whether we should write game logs to a file.
- 	 * @param args
 	 */
+
+	public static void txt2String(File file) {
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String s = null;
+			int i = 0;
+			while ((s = br.readLine()) != null) {
+				if (i == 0) {
+					attrs = s.split("	");
+				} else {
+					String[] tmp = s.split("	");
+
+					arrs.put(i - 1, tmp);
+
+				}
+				i++;
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void getCards() throws Exception{
+		File file = new File("StarCitizenDeck.txt");
+		txt2String(file);
+		return;
+	}
+
+	public static void menu(){
+		System.out.println("Do you want to see past results or play a game?");
+		System.out.println("   1: Print Game Statistics");
+		System.out.println("   2: Play game");
+	}
+	public static void print(String str){
+		System.out.println(str);
+	}
+
 	public static void main(String[] args) {
 
 		boolean writeGameLogsToFile = false; // Should we write game logs to file?
